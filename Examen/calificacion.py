a94a2cdc28664142a64eb25c4dde2739131b8e52
appli <albertoisralg@gmail.com>
2023-03-26 21:20:01 -0600

    una forma de calificar desde la A a la F

--- Examen/calificacion.py
+++ Examen/calificacion.py
@@ -1,8 +1,9 @@
 
 
-calificacion = int(input("Introduce tu calificacion "))
+calificacion = int(input("Introduce tu calificacion ")) #generamos un input con int para numeros enteros 
 
-if calificacion > 60:
+
+if calificacion >=60:   #un verdadero o falso si es mayor de 60 su calificacion aprobo y si no reprobo
     print("Aprobaste")
 else:
     print("Reprobaste")
---
+++ Examen/calificaion_A.py
@@ -0,0 +1,12 @@
+calificacion = int(input("Introduce tu calificacion "))
+
+if calificacion > 90:
+  print('A')
+elif calificacion > 80:
+  print('B')
+elif calificacion > 70:
+  print('C')
+elif calificacion > 60:
+  print('D')
+else:
+  print('F')
