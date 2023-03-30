63d1b9696ec9c7c5972b4fd192073c048112925c
appli <albertoisralg@gmail.com>
2023-03-30 00:14:42 -0600

    entradas para un juego mecanico

--- Feria/entradas.py
+++ Feria/entradas.py
@@ -1,16 +1,13 @@
 #vamos hacer una entrada para subirte  auna atracion donde te piden altura y tickets
 altura = int(input("Dame tu altura "))
 creditos = int(input("Cuantos creditos tienes "))
-con_una_persona = bool(input("Vienes acompañado? "))
+acompanado = input("Vienes acompañado? (si/no) ")
 
 if creditos < 10:
-  print('No tienes los creditos suficientes ')
-elif altura >= 137 and creditos >= 10:
-  print('Disfruta el viaje!')
-elif altura < 137:
-  if altura < 100 or not con_una_persona:
-    print("Todavía no eres lo suficientemente alto para este paseo")
-elif altura >= 100 and  con_una_persona:
+  print('No tienes los creditos suficientes')
+elif altura >= 137:
+  print("¡Disfruta del viaje!")
+elif altura >= 100 and acompanado.lower() == "si": #esta acompañante hace si pone si se convierte en true de lo contrario es false
   print("¡Disfruta del viaje!")
 else:
-  print("ERROR")
+  print("Todavía no eres lo suficientemente alto para este paseo")
