8bd832c9c94f317462c56ae978b5112831b232da
appli <albertoisralg@gmail.com>
2023-04-04 13:59:17 -0600

    Una detencio que repite el castigo 100 veces

---
+++ detencion/detencion.py
@@ -0,0 +1,10 @@
+#usando for la generar un rango que va desde el 1 al 100 una vez termina el rango termina
+#no es como while que se repite si no que repasa uno por uno hasta terminar
+
+
+
+for detencion in range(101):
+    print (f'no volvere a copiar')
+    print(detencion)
+else:
+    print("Termine mi castigo")
