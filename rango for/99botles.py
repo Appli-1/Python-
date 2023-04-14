ddc60266f2434275005a0e979aa250e0b128b323
appli <albertoisralg@gmail.com>
2023-04-14 14:44:08 -0600

    rango for del 99 al 09

--- rango for/99botles.py
+++ rango for/99botles.py
@@ -1,5 +1,6 @@
 
-#generamos un rango que le decimos 
+#generamos un rango for que i con un rango del 99 al 0 y -1 en la ultimo pirnt
+#del 99 al 1 en bucle
 for i in range(99, 0, -1):
   print(f'{i} bottles of beer on the wall')
   print(f'{i} bottles of beer')
